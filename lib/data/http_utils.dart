960e8a1df73ae048a490e1d1fecc8de26e6db9a3
Zer0-01 <anas.zulkifli2001@gmail.com>
2025-02-03 21:39:30 +0800

    feat: create http_utils file

---
+++ lib/data/http_utils.dart
@@ -0,0 +1,28 @@
+import 'package:http/http.dart' as http;
+
+class HttpUtils {
+  static http.Client? _httpClient;
+
+  static void setHttpClient(http.Client client) {
+    _httpClient = client;
+  }
+
+  static void resetHttpClient() {
+    _httpClient = null;
+  }
+
+  static http.Client get client {
+    return _httpClient ?? http.Client();
+  }
+
+  static final _defaultHttpHeaders = {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+  };
+
+  static final _customHttpHeaders = <String, String>{};
+
+  static void addCustomHttpHeaders(String key, String value) {
+    _customHttpHeaders[key] = value;
+  }
+}
