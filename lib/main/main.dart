90a59a113bbaf2d8f89244ad59284a1ea424533f
Zer0-01 <anas.zulkifli2001@gmail.com>
2025-02-03 21:53:19 +0800

    feat: setup logger

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_nur_mobile/configuration/app_logger.dart';
 import 'package:flutter_nur_mobile/presentation/screen/doa/bloc/doa_bloc.dart';
 import 'package:flutter_nur_mobile/presentation/screen/doa/doa_screen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  AppLogger.configure(isProduction: false);
+  final log = AppLogger.getLogger("main.dart");
   runApp(const MyApp());
 }
 
